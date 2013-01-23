a3306b07e29efadc6cde897919243670f4b63693
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-01-23 09:49:59 -0500

    * grouped fields with associated methods * made fields private, instead of default package scope

--- src/beans/UserData.java
+++ src/beans/UserData.java
@@ -3,28 +3,31 @@ package beans;
 // "Beans and Form processing", JSP Tutorial,
 // http://www.jsptut.com/forms.jsp
 public class UserData {
-  String username;
-  String email;
-  int age;
+
+  private String username;
 
   public void setUsername(String value) {
-    username = value;
-  }
-
-  public void setEmail(String value) {
-    email = value;
-  }
-
-  public void setAge(int value) {
-    age = value;
+    this.username = value;
   }
 
   public String getUsername() {
     return username;
   }
 
+  private String email;
+
+  public void setEmail(String value) {
+    email = value;
+  }
+
   public String getEmail() {
     return email;
+  }
+
+  private int age;
+
+  public void setAge(int value) {
+    age = value;
   }
 
   public int getAge() {
