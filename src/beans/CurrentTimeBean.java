d5573b72aca0a7f715b2acb3fa5edaa909d0b139
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-01-23 09:30:24 -0500

    formatting

--- src/beans/CurrentTimeBean.java
+++ src/beans/CurrentTimeBean.java
@@ -2,40 +2,36 @@ package beans;
 
 import java.util.Calendar;
 
-
-// import java.util.Date;
-
 public class CurrentTimeBean {
 
-    public int hours;
-    public int minutes;
+  public int hours;
+  public int minutes;
 
-    public CurrentTimeBean() 
-    {
-        Calendar cal = Calendar.getInstance();
-        // Date now = new Date();
-        
-        // the following methods were deprecated, use Calendar instead
-        // this.hours = now.getHours();
-        // this.minutes = now.getMinutes();
-        
-        hours = cal.get(Calendar.HOUR_OF_DAY);
-        minutes = cal.get(Calendar.MINUTE);
-    }
+  public CurrentTimeBean() {
+    Calendar cal = Calendar.getInstance();
+    // Date now = new Date();
 
-    public int getHours() {
-        return hours;
-    }
+    // the following methods were deprecated, use Calendar instead
+    // this.hours = now.getHours();
+    // this.minutes = now.getMinutes();
 
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
+    hours = cal.get(Calendar.HOUR_OF_DAY);
+    minutes = cal.get(Calendar.MINUTE);
+  }
 
-    public int getMinutes() {
-        return minutes;
-    }
+  public int getHours() {
+    return hours;
+  }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
+  public void setHours(int hours) {
+    this.hours = hours;
+  }
+
+  public int getMinutes() {
+    return minutes;
+  }
+
+  public void setMinutes(int minutes) {
+    this.minutes = minutes;
+  }
 }
